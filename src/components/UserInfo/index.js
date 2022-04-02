a5dbb32cb3e7fa39934edffd51ad8a221978aa36
SandeepRajGanji <deepuganji@gmail.com>
2022-04-02 17:11:53 +0000

    fetching data using blogs api and routing

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      className="profile-img"
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+    />
+    <h1 className="user-name">Sandeep Raj Ganji</h1>
+    <p className="user-designation">
+      Full Stack Developer(MERN Specialization)
+    </p>
+  </div>
+)
+
+export default UserInfo
